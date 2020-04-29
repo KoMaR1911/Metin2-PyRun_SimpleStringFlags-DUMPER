c9552724f0811147b8a2c1c7dac7bd6a9d541eea
Komar Komarowski <galozaur@gmail.com>
2020-04-29 05:44:59 +0200

    Init Commit

---
+++ PyRunSimpleStringFlagsDumperMetin2/PyRunSimpleStringFlagsDumperMetin2/dllmain.cpp
@@ -0,0 +1,50 @@
+﻿// dllmain.cpp : Definiuje punkt wejścia dla aplikacji DLL.
+#include "header.h"
+#include "detours.h"
+#include <iostream>
+#include <io.h>
+
+#pragma comment(lib, "detours.lib")
+
+typedef struct {
+	int cf_flags;  /* bitmask of CO_xxx flags relevant to future */
+} PyCompilerFlags;
+typedef int(__cdecl*tPyRun_SimpleStringFlags)(const char *command, PyCompilerFlags *flags);
+
+tPyRun_SimpleStringFlags nPyRun_SimpleStringFlags;
+int __cdecl hkPyRun_SimpleStringFlags(const char *command, PyCompilerFlags *flags)
+{
+	FILE * fp;
+
+	if (command && fopen_s(&fp, "C:\\dump.txt", "ab+") == 0)
+	{
+		fwrite(command, strlen(command), 1, fp);
+		fclose(fp);
+		printf("Dumped \n");
+	}
+
+	//printf("[Hook] PyRun_SimpleStringFlags: %i %i \n", *command, *flags);
+
+	return nPyRun_SimpleStringFlags(command, flags);
+}
+
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+	{
+		auto oPyRun = tPyRun_SimpleStringFlags(GetProcAddress(GetModuleHandle(L"python27.dll"), "PyRun_SimpleStringFlags"));
+		nPyRun_SimpleStringFlags = (tPyRun_SimpleStringFlags)DetourFunction((PBYTE)oPyRun, (PBYTE)hkPyRun_SimpleStringFlags);
+	}
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
---
+++ PyRunSimpleStringFlagsDumperMetin2/PyRunSimpleStringFlagsDumperMetin2/targetver.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+
+// Dołączenie pliku SDKDDKVer.h definiuje najwyższą dostępną platformę systemu Windows.
+
+// Jeśli chcesz skompilować aplikację dla wcześniejszej platformy systemu Windows, dołącz plik WinSDKVer.h i
+// ustaw makro _WIN32_WINNT na platformę, którą chcesz obsługiwać, przed dołączeniem pliku SDKDDKVer.h.
+
+#include <SDKDDKVer.h>
